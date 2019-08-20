bc8aa69d5d7ef4c7bfb3e864b76672f4eefddb3c
Xiang-Ren Wang <wangallen880117@gmail.com>
2019-08-20 21:31:24 +0800

    Add existing file

---
+++ Simple_Desktop_Pet/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Simple_Desktop_Pet
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.changePictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+			this.animationTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.changePictureToolStripMenuItem,
+			this.exitToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(160, 48);
+			// 
+			// changePictureToolStripMenuItem
+			// 
+			this.changePictureToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.toolStripMenuItem2,
+			this.toolStripMenuItem3,
+			this.toolStripMenuItem4,
+			this.toolStripMenuItem5,
+			this.animationTestToolStripMenuItem});
+			this.changePictureToolStripMenuItem.Name = "changePictureToolStripMenuItem";
+			this.changePictureToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+			this.changePictureToolStripMenuItem.Text = "Change Picture";
+			// 
+			// toolStripMenuItem2
+			// 
+			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(155, 22);
+			this.toolStripMenuItem2.Text = "1";
+			this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+			// 
+			// toolStripMenuItem3
+			// 
+			this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+			this.toolStripMenuItem3.Size = new System.Drawing.Size(155, 22);
+			this.toolStripMenuItem3.Text = "2";
+			this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+			// 
+			// toolStripMenuItem4
+			// 
+			this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+			this.toolStripMenuItem4.Size = new System.Drawing.Size(155, 22);
+			this.toolStripMenuItem4.Text = "3";
+			this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+			// 
+			// toolStripMenuItem5
+			// 
+			this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+			this.toolStripMenuItem5.Size = new System.Drawing.Size(155, 22);
+			this.toolStripMenuItem5.Text = "4";
+			this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+			// 
+			// animationTestToolStripMenuItem
+			// 
+			this.animationTestToolStripMenuItem.Name = "animationTestToolStripMenuItem";
+			this.animationTestToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+			this.animationTestToolStripMenuItem.Text = "AnimationTest";
+			this.animationTestToolStripMenuItem.Click += new System.EventHandler(this.animationTestToolStripMenuItem_Click);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+			this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 33;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// timer2
+			// 
+			this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+			// 
+			// toolTip1
+			// 
+			this.toolTip1.ShowAlways = true;
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.ShowInTaskbar = false;
+			this.Text = "DeskPet";
+			this.TopMost = true;
+			this.TransparencyKey = System.Drawing.SystemColors.Control;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.contextMenuStrip1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem changePictureToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+		private System.Windows.Forms.ToolStripMenuItem animationTestToolStripMenuItem;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Timer timer2;
+		private System.Windows.Forms.ToolTip toolTip1;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+	}
+}
+
---
+++ Simple_Desktop_Pet/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Simple_Desktop_Pet
+{
+
+	public partial class Form1 : Form
+	{
+		private int nOldWndLeft;
+		private int nOldWndTop;
+		private int nClickX;
+		private int nClickY;
+		private bool isDragging;
+
+		Random rng = new Random();
+		int ScreenWidth = Screen.PrimaryScreen.Bounds.Width;
+		int ScreenHeight = Screen.PrimaryScreen.Bounds.Height;
+		private int goalPosLeft;
+		private int goalPosTop;
+
+		private int ImageIdx;
+		private int AnimationIdx;
+		const int frameInterval = 333;
+		const int moveInterval = 3;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			isDragging = false;
+
+			ImageIdx = 1;
+			switchImage();
+
+			AnimationIdx = 0;
+
+			goalPosLeft = rng.Next() % ScreenWidth;
+			goalPosTop = rng.Next() % ScreenHeight;
+
+			timer1.Interval = frameInterval;
+			timer1.Start();
+
+			timer2.Interval = moveInterval;
+			timer2.Start();
+
+			toolTip1.ShowAlways = true;
+			toolTip1.ToolTipTitle = ""; // NULL TITLE
+			toolTip1.SetToolTip(pictureBox1, "Test_Message");
+
+			notifyIcon1.Text = "Text_Message";
+		}
+
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			nOldWndLeft = this.Left;
+			nOldWndTop = this.Top;
+			nClickX = e.X;
+			nClickY = e.Y;
+			isDragging = true;
+		}
+
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (isDragging)
+			{
+				this.Left = e.X + nOldWndLeft - nClickX;
+				this.Top = e.Y + nOldWndTop - nClickY;
+				nOldWndLeft = this.Left;
+				nOldWndTop = this.Top;
+			}
+		}
+
+		private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+		{
+			isDragging = false;
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void toolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			ImageIdx = 1;
+			switchImage();
+		}
+
+		private void toolStripMenuItem2_Click(object sender, EventArgs e)
+		{
+			ImageIdx = 2;
+			switchImage();
+		}
+
+		private void toolStripMenuItem3_Click(object sender, EventArgs e)
+		{
+			ImageIdx = 3;
+			switchImage();
+		}
+
+		private void toolStripMenuItem4_Click(object sender, EventArgs e)
+		{
+			ImageIdx = 4;
+			switchImage();
+		}
+
+		private void animationTestToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			ImageIdx = 5;
+		}
+	
+		private void switchImage()
+		{
+			switch (ImageIdx)
+			{
+				case 1: pictureBox1.Image = Properties.Resources.IMAGE_1; break;
+				case 2: pictureBox1.Image = Properties.Resources.IMAGE_2; break;
+				case 3: pictureBox1.Image = Properties.Resources.IMAGE_3; break;
+				case 4: pictureBox1.Image = Properties.Resources.IMAGE_4; break;
+				default: break;
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			AnimationIdx++;
+			AnimationIdx = AnimationIdx % 2;
+			if( ImageIdx == 5)
+			{
+				switch (AnimationIdx)
+				{
+					case 0: pictureBox1.Image = Properties.Resources.IMAGE_3; break;
+					case 1: pictureBox1.Image = Properties.Resources.IMAGE_4; break;
+					default: break;
+				}
+			}
+		}
+
+		private void timer2_Tick(object sender, EventArgs e)
+		{
+			randomMove();
+		}
+
+		private void randomMove()
+		{
+			if (!isDragging)
+			{
+				String Message;
+				Message = "Top:" + Convert.ToString(this.Top) + "\n"
+						+ "Left:" + Convert.ToString(this.Left) + "\n"
+						+ "GTop:" + Convert.ToString(goalPosTop) + "\n"
+						+ "GLeft:" + Convert.ToString(goalPosLeft);
+
+				// Message Update
+				toolTip1.SetToolTip(pictureBox1, Message);
+				notifyIcon1.Text = Message; // Can't Over 64 byte
+
+				// Console Message
+				Console.WriteLine("CurrentTop: "+Convert.ToString(this.Top));
+				Console.WriteLine("CurrentLeft: "+Convert.ToString(this.Left));
+				Console.WriteLine("Goal Top: "+Convert.ToString(goalPosTop));
+				Console.WriteLine("Goal Left: "+Convert.ToString(goalPosLeft));
+
+				if (this.Left == goalPosLeft && this.Top == goalPosTop)
+				{
+					goalPosLeft = rng.Next() % ScreenWidth;
+					goalPosTop = rng.Next() % ScreenHeight;
+				}
+
+				if (this.Left < goalPosLeft) this.Left++;
+				else if (this.Left > goalPosLeft) this.Left--;
+
+				if (this.Top < goalPosTop) this.Top++;
+				else if (this.Top > goalPosTop) this.Top--;
+			}
+		}	}
+
+
+}
